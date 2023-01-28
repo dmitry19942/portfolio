4ea79b640330d4d84c12e3b44382e5ad5a1cae7b
dmitry19942 <gorustovich2013@gmail.com>
2023-01-28 04:19:20 +0300

    adding style title

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -8,7 +8,9 @@ function Projects() {
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
-                <h2 className={style.title}>My Projects</h2>
+                <div>
+                    <h2 className={style.title}>My Projects</h2>
+                </div>
                 <div className={style.projects}>
                     <Project title={'ToDoList'} description={'a list of errands and other tasks – often written on a piece of paper as a memory aid – that one needs or intends to accomplish.'} onClick={"https://dmitry19942.github.io/todolist/"}/>
                     <Project title={'Counter'} description={'is a mechanical or electronic device which keeps a count of something and displays the total.'} onClick={'https://dmitry19942.github.io/counter/'}/>
