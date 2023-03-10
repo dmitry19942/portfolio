0a61bb7053677571ff2c5256199350ef9cd04c99
dmitry19942 <gorustovich2013@gmail.com>
2023-03-10 19:39:42 +0300

    add node-sass

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,4 +1,4 @@
-import style from './Projects.module.css';
+import style from './Projects.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
 import Project from "./project/Project";
 import Title from "../common/components/title/Title";
