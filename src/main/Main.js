5bc53318a8ee5cb619e0f3e6663c38a16cadfa2e
dmitry19942 <gorustovich2013@gmail.com>
2023-03-23 18:20:50 +0300

    adding scss file for component Main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,19 +1,24 @@
-import style from './Main.module.css';
+import style from './Main.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
+import photoImage from '../assets/photo/photo.jpg'
+
 
 
 function Main() {
+
+    const photo = {
+        backgroundImage: `url(${photoImage}`,
+    };
+
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={style.text}>
-                    <span>Hi There</span>
-                    <h1>I am Dzmitry Harustovich</h1>
-                    <p>Frontend Developer.</p>
+                    <h5>HELLO, MY NAME IS</h5>
+                    <h1>DZMITRY HARUSTOVICH</h1>
+                    <h5>FRONT-END DEVELOPER</h5>
                 </div>
-                <div className={style.photo}>
-
-                </div>
+                <div className={style.photo} style={photo}></div>
             </div>
         </div>
     );
