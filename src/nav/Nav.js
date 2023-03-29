3c2b351378db6ba1fc2c7def32896b1bb6cbcd8e
dmitry19942 <gorustovich2013@gmail.com>
2023-03-29 20:25:12 +0300

    adding burgerButton in Header component

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -7,6 +7,13 @@ function Nav() {
             <a className={style.a} href={"#skills"}>Skills</a>
             <a className={style.a} href={"#projects"}>Projects</a>
             <a className={style.a} href={"#contacts"}>Contacts</a>
+            <div>
+                <div className={style.burgerBtn}>
+                    <svg className={style.svg} aria-hidden='true' focusable='false' data-prefix='fas' data-icon='bars' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+                        <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
+                    </svg>
+                </div>
+            </div>
         </div>
     );
 }
