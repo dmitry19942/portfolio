3f18d2cf44cf0d429ae4bce7506ad536a2e96f40
dmitry19942 <gorustovich2013@gmail.com>
2023-04-10 20:21:06 +0300

    update component Contacts and adding scss file

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,18 +1,19 @@
-import style from './Contacts.module.css';
+import style from './Contacts.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
+import Title from "../common/components/title/Title";
 
 
 function Contacts() {
     return (
         <div id='contacts' className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <h2 className={style.title}>Contacts</h2>
+                <Title text={'Contacts'}/>
                 <form action="" className={style.formContainer}>
-                        <input type="text" className={style.input}/>
-                        <input type="text" className={style.input}/>
-                        <textarea name="" id="" cols="30" rows="5" className={style.textarea}></textarea>
+                        <input type="text" className={style.input} placeholder={'Your Name'}/>
+                        <input type="text" className={style.input2} placeholder={'Your Email'}/>
+                        <textarea name="" id="" cols="30" rows="5" className={style.textarea} placeholder={'Your Message'}></textarea>
                 </form>
-                <button type='submit' className={style.button}>Send message</button>
+                <button type='submit' className={style.button}>SEND MESSAGE</button>
             </div>
         </div>
     );
