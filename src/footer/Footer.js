a14f0846c9b20cbdbae7900e3431ca8f950a8aec
dmitry19942 <gorustovich2013@gmail.com>
2023-04-13 18:14:06 +0300

    update component Footer and adding scss file

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,19 +1,50 @@
-import style from './Footer.module.css';
+import style from './Footer.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
-
+import telegramIcon from "../assets/icon/Telegram_logo.png";
+import facebookIcon from "../assets/icon/facebookLogo.png";
+import linkedinIcon from "../assets/icon/linkedinLogo.png";
+import gmailIcon from "../assets/icon/gmailLogo.png";
 
 function Footer() {
+
+    const telegram = {
+        backgroundImage: `url(${telegramIcon}`,
+    };
+
+    const facebook = {
+        backgroundImage: `url(${facebookIcon}`,
+    };
+
+    const linkedin = {
+        backgroundImage: `url(${linkedinIcon}`,
+    };
+
+    const gmail = {
+        backgroundImage: `url(${gmailIcon}`,
+    };
+
+    const openInNewTab = url => {
+        window.open(url);
+    };
+
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
-                <h2 className={style.title}>Dzmitry Harustovich</h2>
+                <a href={"#main"} className={style.a}>
+                    <h2 className={style.title}>Dzmitry Harustovich</h2>
+                </a>
                 <div className={style.icons}>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
+                    <a onClick={() => openInNewTab('https://t.me/dmitriy19942')}>
+                        <div className={style.icon} style={telegram}></div>
+                    </a>
+                    <a onClick={() => openInNewTab('https://www.facebook.com/profile.php?id=100050173746359&ref=bookmarks')}><div className={style.icon} style={facebook}></div>
+                    </a>
+                    <div className={style.icon} style={linkedin}></div>
+                    <a onClick={() => openInNewTab('mailto:gorustovich2013@gmail.com')}>
+                        <div className={style.icon} style={gmail}></div>
+                    </a>
                 </div>
-                <span>© 2023, All Rights Reserved.</span>
+                <span className={style.span}>© 2023, All Rights Reserved.</span>
             </div>
         </div>
     );
