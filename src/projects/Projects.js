600f32d089d52f5c0a571b02317339c6d5e185a2
dmitry19942 <gorustovich2013@gmail.com>
2023-04-25 17:22:33 +0300

    adding method map and adding hover for buttons

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -6,32 +6,27 @@ import todoImage from './../assets/image/todolist.jpg'
 import counterImage from './../assets/image/counter.jpg'
 import socialNetworkImage from './../assets/image/socialNetwork.png'
 import reactTasksImage from './../assets/image/reactTasks.jpg'
+import futureProjectImage1 from './../assets/image/futureProject.jpg'
+import futureProjectImage2 from './../assets/image/futureProject2.webp'
 
 
 function Projects() {
 
-    const todolist = {
-        backgroundImage: `url(${todoImage}`,
-    };
-    const counter = {
-        backgroundImage: `url(${counterImage}`,
-    };
-    const socialNetwork = {
-        backgroundImage: `url(${socialNetworkImage}`,
-    };
-    const reactTasks = {
-        backgroundImage: `url(${reactTasksImage}`,
-    };
+    const projects = [
+        {style: {backgroundImage: `url(${todoImage}`}, title: 'ToDoList', description: 'is a list of errands and other tasks – often written on a piece of paper as a memory aid – that one needs or intends to accomplish.', onClick: 'https://dmitry19942.github.io/todolist/'},
+        {style: {backgroundImage: `url(${socialNetworkImage}`}, title: 'Social Network', description: 'is a dedicated website or other application which enables users to communicate with each other by posting information, comments, messages, images, etc.', onClick: 'https://dmitry19942.github.io/social-network/'},
+        {style: {backgroundImage: `url(${reactTasksImage}`}, title: 'React Tasks', description: 'developing the latest user-facing features using React.', onClick: 'https://dmitry19942.github.io/simple-react-homeworks-master/#/pre-junior'},
+        {style: {backgroundImage: `url(${counterImage}`}, title: 'Counter', description: 'is a mechanical or electronic device which keeps a count of something and displays the total.', onClick: 'https://dmitry19942.github.io/counter/'},
+        {style: {backgroundImage: `url(${futureProjectImage1}`}, title: 'Future Project', description: '... in progress', onClick: 'https://github.com/dmitry19942'},
+        {style: {backgroundImage: `url(${futureProjectImage2}`}, title: 'Future Project', description: '... in progress', onClick: 'https://github.com/dmitry19942'},
+    ]
 
     return (
         <div id='projects' className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title text={'My Projects'}/>
                 <div className={style.projects}>
-                    <Project style={todolist} title={'ToDoList'} description={'a list of errands and other tasks – often written on a piece of paper as a memory aid – that one needs or intends to accomplish.'} onClick={"https://dmitry19942.github.io/todolist/"}/>
-                    <Project style={counter} title={'Counter'} description={'is a mechanical or electronic device which keeps a count of something and displays the total.'} onClick={'https://dmitry19942.github.io/counter/'}/>
-                    <Project style={socialNetwork} title={'Social Network'} description={'a dedicated website or other application which enables users to communicate with each other by posting information, comments, messages, images, etc.'} onClick={'https://dmitry19942.github.io/social-network/'}/>
-                    <Project style={reactTasks} title={'React Tasks'} description={'developing the latest user-facing features using React.'} onClick={'https://dmitry19942.github.io/simple-react-homeworks-master/#/pre-junior'}/>
+                    {projects.map(p => <Project style={p.style} title={p.title} description={p.description} onClick={p.onClick} />)}
                 </div>
             </div>
         </div>
--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -18,71 +18,27 @@ import gitIcon from "../assets/icon/git.png";
 
 function Skills() {
 
-    const js = {
-        backgroundImage: `url(${jsIcon}`,
-    };
-
-    const react = {
-        backgroundImage: `url(${reactIcon}`,
-    };
-
-    const typescript = {
-        backgroundImage: `url(${typescriptIcon}`,
-    };
-
-    const redux = {
-        backgroundImage: `url(${reduxIcon}`,
-    };
-
-    const axios = {
-        backgroundImage: `url(${axiosIcon}`,
-    };
-
-    const restAPI = {
-        backgroundImage: `url(${restAPIIcon}`,
-    };
-
-    const unitTest = {
-        backgroundImage: `url(${unitTestIcon}`,
-    };
-
-    const htmlCss = {
-        backgroundImage: `url(${htmlCssIcon}`,
-    };
-
-    const storybook = {
-        backgroundImage: `url(${storybookIcon}`,
-    };
-
-    const materialUI = {
-        backgroundImage: `url(${materialUIIcon}`,
-    };
-
-    const antDesign = {
-        backgroundImage: `url(${antDesignIcon}`,
-    };
-
-    const git = {
-        backgroundImage: `url(${gitIcon}`,
-    };
+    const skills = [
+        {style: {backgroundImage: `url(${jsIcon}`}, title: 'JS', description: 'is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat.'},
+        {style: {backgroundImage: `url(${htmlCssIcon}`}, title: 'HTML5&CSS3', description: 'HTML describes the structure of the pages, primarily in regards to tables, text, headings, and images or graphics. It’s the standard programming language for the overall appearance of web pages. CSS, on the other hand, is the language used for describing the presentation of each page, and primarily in regards to the layout, fonts, and colors.'},
+        {style: {backgroundImage: `url(${reactIcon}`}, title: 'REACT', description: 'is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.'},
+        {style: {backgroundImage: `url(${reduxIcon}`}, title: 'REDUX', description: 'is a pattern and library for managing and updating application state, using events called "actions". It serves as a centralized store for state that needs to be used across your entire application, with rules ensuring that the state can only be updated in a predictable fashion.'},
+        {style: {backgroundImage: `url(${typescriptIcon}`}, title: 'TYPESCRIPT', description: 'is a syntactic superset of JavaScript which adds static typing. This basically means that TypeScript adds syntax on top of JavaScript, allowing developers to add types.'},
+        {style: {backgroundImage: `url(${axiosIcon}`}, title: 'AXIOS', description: 'is a Javascript library used to make HTTP requests from node. js or XMLHttpRequests from the browser and it supports the Promise API that is native to JS ES6.'},
+        {style: {backgroundImage: `url(${restAPIIcon}`}, title: 'REST API', description: 'is an application programming interface (API or web API) that conforms to the constraints of REST architectural style and allows for interaction with RESTful web services.'},
+        {style: {backgroundImage: `url(${unitTestIcon}`}, title: 'UNIT TESTS', description: 'is a way of testing a unit - the smallest piece of code that can be logically isolated in a system.'},
+        {style: {backgroundImage: `url(${storybookIcon}`}, title: 'STORYBOOK', description: 'is a development environment for UI components that enables you to create and showcase components in an environment outside your main application.'},
+        {style: {backgroundImage: `url(${materialUIIcon}`}, title: 'MATERIAL UI', description: `is an open-source React component library that implements Google's Material Design. It includes a comprehensive collection of prebuilt components that are ready for use in production right out of the box.`},
+        {style: {backgroundImage: `url(${antDesignIcon}`}, title: 'ANT DESIGN', description: 'is a React UI library that has a plethora of easy-to-use components that are useful for building elegant user interfaces.'},
+        {style: {backgroundImage: `url(${gitIcon}`}, title: 'GIT', description: 'is a free and open source distributed code management and Version control system that is distributed under the GNU General Public License version 2. In addition to software version control, Git is used for other applications including configuration management and content management.'},
+    ]
 
     return (
         <div id='skills' className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text={'My Skills'} />
                 <div className={style.skills}>
-                    <Skill style={js} title={'JS'} description={'is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat.'}/>
-                    <Skill style={htmlCss} title={'HTML5&CSS3'} description={'HTML describes the structure of the pages, primarily in regards to tables, text, headings, and images or graphics. It’s the standard programming language for the overall appearance of web pages. CSS, on the other hand, is the language used for describing the presentation of each page, and primarily in regards to the layout, fonts, and colors.'}/>
-                    <Skill style={react} title={'REACT'} description={'is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.'}/>
-                    <Skill style={redux} title={'REDUX'} description={'is a pattern and library for managing and updating application state, using events called "actions". It serves as a centralized store for state that needs to be used across your entire application, with rules ensuring that the state can only be updated in a predictable fashion.'}/>
-                    <Skill style={typescript} title={'TYPESCRIPT'} description={'is a syntactic superset of JavaScript which adds static typing. This basically means that TypeScript adds syntax on top of JavaScript, allowing developers to add types.'}/>
-                    <Skill style={axios} title={'AXIOS'} description={'is a Javascript library used to make HTTP requests from node. js or XMLHttpRequests from the browser and it supports the Promise API that is native to JS ES6.'}/>
-                    <Skill style={restAPI} title={'REST API'} description={'is an application programming interface (API or web API) that conforms to the constraints of REST architectural style and allows for interaction with RESTful web services'}/>
-                    <Skill style={unitTest} title={'UNIT TESTS'} description={'is a way of testing a unit - the smallest piece of code that can be logically isolated in a system.'}/>
-                    <Skill style={storybook} title={'STORYBOOK'} description={'is a development environment for UI components that enables you to create and showcase components in an environment outside your main application.'}/>
-                    <Skill style={materialUI} title={'MATERIAL UI'} description={`is an open-source React component library that implements Google's Material Design. It includes a comprehensive collection of prebuilt components that are ready for use in production right out of the box.`}/>
-                    <Skill style={antDesign} title={'ANT DESIGN'} description={'is a React UI library that has a plethora of easy-to-use components that are useful for building elegant user interfaces.'}/>
-                    <Skill style={git} title={'GIT'} description={'is a React UI library that has a plethora of easy-to-use components that are useful for building elegant user interfaces.'}/>
+                    {skills.map(s => <Skill style={s.style} title={s.title} description={s.description} /> )}
                 </div>
             </div>
         </div>
