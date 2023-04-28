c36b1e148ed2f0bd2cf0aaf5342062b14b42167e
dmitry19942 <gorustovich2013@gmail.com>
2023-04-28 22:30:01 +0300

    fix open and close menu in header

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,24 @@
 import style from './Header.module.scss';
 import Nav from "../nav/Nav";
+import {useState} from "react";
 
 function Header() {
+
+    const [menu, setMenu] = useState(false)
+
+    const openMenu = () => {
+        setMenu(true)
+    }
+
+    const closeMenu = () => {
+        setMenu(false)
+    }
+
     return (
         <div className={style.header}>
+            {menu && <div className={style.background} onClick={closeMenu}/>}
             <div className={style.navigation}>
-                <Nav/>
+                <Nav menu={menu} openMenu={openMenu} closeMenu={closeMenu}/>
             </div>
         </div>
     );
