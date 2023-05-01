be79e047aa2233f9f3db8acaebb42bc3534049dd
dmitry19942 <gorustovich2013@gmail.com>
2023-05-01 17:18:12 +0300

    adding form for component RemoteWork

--- src/remotework/RemoteWork.js
+++ src/remotework/RemoteWork.js
@@ -1,14 +1,43 @@
 import style from './RemoteWork.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
 import Title from "../common/components/title/Title";
+import {useState} from "react";
+import HiringModal from "../common/components/modalWindow/ModalWindow";
+
 
 
 function RemoteWork() {
+
+    const [activeModal, setActiveModal] = useState(false)
+
+    const handlerModal = () => {
+        setActiveModal(true)
+    }
+
     return (
         <div className={style.remoteWorkBlock}>
             <div className={`${styleContainer.container} ${style.remoteWorkContainer}`}>
                 <Title text={'I Am Available For Freelance'}/>
-                <button className={style.button}>HIRE ME</button>
+                <button className={style.button} onClick={handlerModal}>HIRE ME</button>
+                <HiringModal active={activeModal} setActive={setActiveModal}>
+                    <div>
+                        <div className={style.header}>
+                            <h4>HAVE A PROJECT?</h4>
+                            <p>I’m ready to help you. You just type details below,and/or send us a file.</p>
+                        </div>
+                        <form className={style.form}>
+                            <div className={style.inputBlock}>
+                                <input type="text" placeholder="Your Name"/>
+                                <input type="text" placeholder="Your Project title"/>
+                                <input type="email" placeholder="Please enter Your Email"/>
+                            </div>
+                            <textarea placeholder="Your Message..."/>
+                                <button className={style.button}>
+                                    Send Offer
+                                </button>
+                        </form>
+                    </div>
+                </HiringModal>
             </div>
         </div>
     );
